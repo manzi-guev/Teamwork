6aedf3e4aaff222d0b126bb92d7ef3f1658c0d20
Manzi Guevara <manziguevara@gmail.com>
2019-10-01 11:05:42 +0200

    declared variables using es6 [Finishes #168839162]

--- UI/JS/main.js
+++ UI/JS/main.js
@@ -1,27 +1,27 @@
-var modal = document.getElementById('simpleModal');
-var modalBtn = document.getElementById('modalBtn');
-var closedBtn = document.getElementsByClassName('closedBtn')[0];
+const modal = document.getElementById('simpleModal');
+const modalBtn = document.getElementById('modalBtn');
+const closedBtn = document.getElementsByClassName('closedBtn')[0];
 
+let openModal = () => {
+  modal.style.display = 'block';
+};
 modalBtn.addEventListener('click', openModal);
+
+let closedModal = () => {
+  modal.style.display = 'none';
+};
 closedBtn.addEventListener('click', closedModal);
 
-function openModal() {
-  modal.style.display = 'block';
-}
-function closedModal() {
-  modal.style.display = 'none';
-}
+const modal1 = document.getElementById('simpleModal1');
+const modalBtn1 = document.getElementById('modalBtn1');
+const closedBtn1 = document.getElementsByClassName('closedBtn1')[0];
 
-var modal1 = document.getElementById('simpleModal1');
-var modalBtn1 = document.getElementById('modalBtn1');
-var closedBtn1 = document.getElementsByClassName('closedBtn1')[0];
-
-modalBtn1.addEventListener('click', openModal1);
-closedBtn1.addEventListener('click', closedModal1);
-
-function openModal1() {
+const openModal1 = () => {
   modal1.style.display = 'block';
-}
-function closedModal1() {
+};
+modalBtn1.addEventListener('click', openModal1);
+
+const closedModal1 = () => {
   modal1.style.display = 'none';
-}
+};
+closedBtn1.addEventListener('click', closedModal1);
