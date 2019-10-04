b47341ba84c7143a42cf771273f0d432f3135422
Manzi Guevara <manziguevara@gmail.com>
2019-10-04 13:24:35 +0200

    working on middlewares and correcting them

--- server/middleware/validation.middleware.js
+++ server/middleware/validation.middleware.js
@@ -1,29 +1,138 @@
 import Joi from '@hapi/joi';
 import helping from '../helpers/validation.helper';
+import { signup, signin } from '../helpers/validation.helper';
 
+// const validation = (req, res, next) => {
+//   const methodsSupported = ['post', 'put', 'patch', 'get', 'delete'];
+//   const { pathing } = req.params;
+//   const paths = [{ path: '/api/v1/auth/signup/' }];
+//   const method = req.method.toLowerCase();
 
-const validation = (req, res, next) => {
-  const methodsSupported = ['post', 'put', 'patch'];
-  const { pathing } = req.params;
-  const paths = [{ path: '/api/v1/auth/signup/' }];
-  const method = req.method.toLowerCase();
-
-  const check = paths.filter(checkroute => checkroute.path === pathing);
-  if (check) {
-    next();
-  } else {
+//   const check = paths.filter(checkroute => checkroute.path === pathing);
+//   if (check) {
+//     next();
+//   } else {
+//     return res.status(400).json({
+//       status: 400,
+//       error: 'Bad request'
+//     });
+//   }
+//   if (methodsSupported.includes(method) != undefined) {
+//     next();
+//   }
+//   return res.status(405).json({
+//     status: 405,
+//     error: 'Method not found'
+//   });
+// };
+const signUp = (req, res, next) => {
+  const schema = {
+    firstname: Joi.string()
+      .strict()
+      .trim()
+      .required(),
+    lastname: Joi.string()
+      .strict()
+      .trim()
+      .required(),
+    email: Joi.string()
+      .strict()
+      .trim()
+      .required()
+      .email(),
+    password: Joi.string()
+      .strict()
+      .required()
+      .trim()
+      .min(5),
+    gender: Joi.string()
+      .strict()
+      .trim()
+      .required(),
+    jobRole: Joi.string()
+      .strict()
+      .trim()
+      .required(),
+    department: Joi.string()
+      .strict()
+      .trim()
+      .required(),
+    address: Joi.string()
+      .strict()
+      .trim()
+      .required()
+  };
+  const output = Joi.validate(req.body, schema);
+  if (output.error != null) {
     return res.status(400).json({
       status: 400,
-      error: 'Bad request'
+      error: `${output.error.details[0].message}`
     });
   }
-  if (methodsSupported.includes(method) !== undefined) {
-    next();
+  next();
+};
+const signIn = (req, res, next) => {
+  const schema = {
+    email: Joi.string()
+      .strict()
+      .trim()
+      .required()
+      .email(),
+    password: Joi.string()
+      .strict()
+      .trim()
+      .required()
+      .min(5)
+  };
+  const output = Joi.validate(req.body, schema);
+  if (output.error != null) {
+    return res.status(400).json({
+      status: 400,
+      error: `${output.error.details[0].message}`
+    });
   }
-  return res.status(405).json({
-    status: 405,
-    error: 'Method not found'
-  });
+  next();
+};
+const createArticle = (req, res, next) => {
+  const schema = {
+    title: Joi.string()
+      .strict()
+      .trim()
+      .required(),
+    author: Joi.string()
+      .strict()
+      .trim()
+      .required()
+      .min(5),
+    article: Joi.string()
+      .strict()
+      .required()
+      .trim()
+  };
+  const output = Joi.validate(req.body, schema);
+  if (output.error != null) {
+    return res.status(400).json({
+      status: 400,
+      error: `${output.error.details[0].message}`
+    });
+  }
+  next();
+};
+const createComment = (req, res, next) => {
+  const schema = {
+    comments: Joi.string()
+      .strict()
+      .trim()
+      .required()
+  };
+  const output = Joi.validate(req.body, schema);
+  if (output.error != null) {
+    return res.status(400).json({
+      status: 400,
+      error: `${output.error.details[0].message}`
+    });
+  }
+  next();
 };
 
-export default validation;
+export { signUp, signIn, createArticle, createComment };
