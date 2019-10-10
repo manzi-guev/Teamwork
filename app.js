7d8733d20cdae1ed1153d6064a5aca867aedb593
Manzi Guevara <manziguevara@gmail.com>
2019-10-10 23:14:06 +0200

    Integrate TravisCI and Coveralls

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
 
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    status: 200,
+    message: 'Welcome to TEAMWORK PROJECT'
+  });
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
