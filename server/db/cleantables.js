e07b451c6771210b4b5df279648877ad88e43589
Manzi Guevara <manziguevara@gmail.com>
2019-10-11 05:38:27 +0200

    ch(ui): UI final updates [Finishes #169081792]

--- server/db/cleantables.js
+++
@@ -1,7 +0,0 @@
-import connec from '../db/dbconnection';
-const dropTables = async () => {
-  await connec.query('drop table articles');
-  await connec.query('delete from users');
-};
-dropTables();
-export default dropTables;
